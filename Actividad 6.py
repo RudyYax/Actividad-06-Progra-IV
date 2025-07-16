9b304cbc1df2a771e944cd6c63b65c69777e1204
Rudy <rudyax2004@gmail.com>
2025-07-15 22:26:49 -0600

    tercer Commit ACTIVIDAD 6

--- Actividad 6.py
+++ Actividad 6.py
@@ -4,28 +4,27 @@ print("----INVENTARIO DE UNA TIENDA----")
 productos = int(input(f" \n Ingrese la cantidad de productos que desea ingresar: "))
 for i in range(productos):
     print(f"\n Ingrese el producto {i+1}: ")
-    while(productos > 0):
-        print(f"\n Ingrese los siguientes datos")
-        codigo = input(f" \n Ingrese el codigo del producto: ")
-        if codigo == codigo:
-            print(f"\n Codigo ya ingresado")
-            break
+    print(f"\n Ingrese los siguientes datos")
+    codigo = input(f" \n Ingrese el codigo del producto: ")
+    if codigo == codigo:
+        print(f"\n Codigo ya ingresado")
+        nombre = input(f" \n Ingrese el nombre del producto: ")
+        precio = input(f" \n Ingrese el precio del producto: ")
+        if precio > 0:
+            print(f"\n precio ingresado correctamente")
         else:
-            nombre = input(f" \n Ingrese el nombre del producto: ")
-            precio = input(f" \n Ingrese el precio del producto: ")
-            if precio > 0:
-                print(f"\n precio ingresado correctamente")
-            else:
-                print(f"\n El precio no puede ser menor a 0")
-            cantidad = int(input(f" \n Ingrese la cantidad del producto: "))
-            if cantidad > 0:
-                print(f"\n cantidad ingresado correctamente")
-            else :
-                print("La cantidad ingresad al inventario tiene que ser positiva")
+            print(f"\n El precio no puede ser menor a 0")
+        cantidad = int(input(f" \n Ingrese la cantidad del producto: "))
+        if cantidad > 0:
+            print(f"\n cantidad ingresado correctamente")
+        else:
+            print("La cantidad ingresad al inventario tiene que ser positiva")
         productos[codigo] = {
-            "nombre": nombre,
-            "precio": precio,
-            "cantidad": cantidad
-
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad
         }
+    else:
+        print("codigo ya ingresado")
+
 
