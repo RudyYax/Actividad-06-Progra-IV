e8d8b498957d21063adda2cce577ae926b0b7480
Rudy <rudyax2004@gmail.com>
2025-07-15 22:20:30 -0600

    segundo Commit ACTIVIDAD 6

--- Actividad 6.py
+++ Actividad 6.py
@@ -11,6 +11,21 @@ for i in range(productos):
             print(f"\n Codigo ya ingresado")
             break
         else:
-            productos -= 1
+            nombre = input(f" \n Ingrese el nombre del producto: ")
+            precio = input(f" \n Ingrese el precio del producto: ")
+            if precio > 0:
+                print(f"\n precio ingresado correctamente")
+            else:
+                print(f"\n El precio no puede ser menor a 0")
+            cantidad = int(input(f" \n Ingrese la cantidad del producto: "))
+            if cantidad > 0:
+                print(f"\n cantidad ingresado correctamente")
+            else :
+                print("La cantidad ingresad al inventario tiene que ser positiva")
+        productos[codigo] = {
+            "nombre": nombre,
+            "precio": precio,
+            "cantidad": cantidad
 
+        }
 
