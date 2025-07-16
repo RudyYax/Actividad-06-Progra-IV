53193f321d1185abe4974162a301dbf4ace04c82
Rudy <rudyax2004@gmail.com>
2025-07-15 22:14:17 -0600

    PRIMER Commit ACTIVIDAD 6

--- Actividad 6.py
+++ Actividad 6.py
@@ -1,2 +1,16 @@
+productos = {}
 print("Actividad 06")
 print("----INVENTARIO DE UNA TIENDA----")
+productos = int(input(f" \n Ingrese la cantidad de productos que desea ingresar: "))
+for i in range(productos):
+    print(f"\n Ingrese el producto {i+1}: ")
+    while(productos > 0):
+        print(f"\n Ingrese los siguientes datos")
+        codigo = input(f" \n Ingrese el codigo del producto: ")
+        if codigo == codigo:
+            print(f"\n Codigo ya ingresado")
+            break
+        else:
+            productos -= 1
+
+
